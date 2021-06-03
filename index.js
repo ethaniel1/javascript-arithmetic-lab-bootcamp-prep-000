8d2c2815125746d5fe3950ec16a0b068a9abbddf
Jonathan  Zylberberg <zylberberg.jonathan@gmail.com>
2021-06-03 22:20:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-function add (a,b) {
+function add(a,b) {
 return a+b } 
 
-function subtract (a,b) {
+function subtract(a,b) {
 return a-b}
 }
 
-function multiply (a,b) {
+function multiply(a,b) {
 return a*b }
 }
 
-function divide (a,b) {
+function divide(a,b) {
 return a/b
 }
 
-function increment (n) {
+function increment(n) {
 return n+= 1
 }
 
-function decrement (n) {
+function decrement(n) {
 return n-= 1
 }
 
-function makeInt (string) {
-return parseInt (string, 10)
+function makeInt(string) {
+return parseInt(string, 10)
 }
 
-function preserveDecimal (string) {
-return parseFloat (string)
+function preserveDecimal(string) {
+return parseFloat(string)
 }
